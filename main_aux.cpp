771a67f925ec8b3dd4b830ae7dbd501772fa9f2e
MatanDev <matan@irobinhood.org>
2016-05-06 19:51:36 +0300

    Finished adding comments to the "main aux"

--- main_aux.cpp
+++ main_aux.cpp
@@ -33,11 +33,103 @@
 
 //TODO - move structures definitions to this file
 
-//shared variables
-settings* allSettings = NULL;
-imagesDatabase* allImagesDatabase = NULL;
-imageData** imagesData = NULL;
+/*----------global variables---------*/
 
+/*
+ * A settings structure that holds the input settings
+ */
+settings* allSettings = NULL;
+/*
+ * An array of pointers that holds the data processed from the images in out database
+ */
+imageData** imagesData = NULL;
+/*
+ * A pointer to a structure that holds the images data manipulated in way that is convenient to use with the "sp_image_proc_utils"
+ */
+imagesDatabase* allImagesDatabase = NULL;
+
+
+
+
+/**
+ * The method gets an image and frees the memory allocated to each one of its parts, and than free the memory for his pointer.
+ * @image - a pointer to the image data
+ */
+void freeImageData(imageData* image)
+{
+	int i;
+	if (image != NULL)
+	{
+		//free the rgbHist matrix
+		if (image->rgbHist != NULL)
+		{
+			for (i=0;i<3;i++)
+			{
+				free(image->rgbHist[i]);
+			}
+			free(image->rgbHist);
+		}
+		//free the sift descriptors matrix
+		if (image->siftDesc != NULL)
+		{
+			for (i=0;i<image->nFeatures;i++)
+			{
+				free(image->siftDesc[i]);
+			}
+			free(image->siftDesc);
+		}
+		//free the pointer to the image
+		free(image);
+	}
+}
+
+/**
+ * The method frees the memory allocated to each image from the loaded images, and to the array itself
+ */
+void freeImages()
+{
+	int i;
+	if (imagesData != NULL)
+	{
+		for (i=0;i<allSettings->numOfImages;i++)
+		{
+			freeImageData(imagesData[i]);
+		}
+	}
+	free(imagesData);
+}
+
+/*
+ * The method frees the memory allocated for the images database structure,
+ * as it hold a re-ordered version of the data from images data
+ */
+void freeDatabase()
+{
+	if (allImagesDatabase != NULL)
+	{
+		if (allImagesDatabase->databaseFeatures != NULL)
+		{
+			free(allImagesDatabase->databaseFeatures);
+		}
+		if (allImagesDatabase->featuresPerImage != NULL)
+		{
+			free(allImagesDatabase->featuresPerImage);
+		}
+		free(allImagesDatabase);
+	}
+}
+
+/*
+ * The method is responsible for freeing the memory allocated to the images data.
+ */
+void freeImagesAndDatabase(){
+	freeDatabase();
+	freeImages();
+}
+
+/*
+ * The method is responsible for freeing the memory allocated to the settings global variable.
+ */
 void clearSettings()
 {
 	int i;
@@ -55,20 +147,37 @@ void clearSettings()
 	}
 }
 
+/*
+ * The method reports the user for an error and terminates the program
+ * the message clears the global variables allocated memory.
+ * @message - the error message that is printed to the user
+ */
 void reportErrorAndExit(const char* message)
 {
 	printf(message);
 	flushNull
-	if (allSettings != NULL)
+	if (allSettings != NULL){
+		freeImagesAndDatabase();
 		clearSettings();
+	}
 	exit(-1); //TODO - check what error code should we return
 }
 
+/*
+ * The method reports the user for an allocation error and terminates the program
+ */
 void reportAllocationErrorAndExit()
 {
 	reportErrorAndExit(ALLOCATION_ERROR_MESSAGE);
 }
 
+/*
+ * A bridge for allocating memory, the method acts as 'calloc' does,
+ * but prompt of failure and exists the program if it could not allocate the requested memory
+ * @itemsCount - the number of items to be allocated
+ * @sizeOfItem - the size of each item
+ * @returns - a void pointer to the allocated memory
+ */
 void* safeCalloc(int itemsCount, int sizeOfItem)
 {
 	void* memoryPointer = calloc(itemsCount, sizeOfItem);
@@ -77,6 +186,10 @@ void* safeCalloc(int itemsCount, int sizeOfItem)
 	return memoryPointer;
 }
 
+/*
+ * This method sets the images database structure using the imagedData array.
+ * This is just a manipulation of how the imaged data is stored, for an convenient use lated on.
+ */
 void setImagesDatabase()
 {
 	int i;
@@ -95,6 +208,12 @@ void setImagesDatabase()
 	}
 }
 
+/*
+ * The method requests the user to input a positive integer,
+ * and update its input to the "value" item given as a pointer.
+ * @message_type - a message suffix presented to the user before the input
+ * @value - a pointer to an integer, where the result would be saved
+ */
 void getAsPositiveInt(const char* message_type, int* value)
 {
 	printf(ENTER_NUMBER_OF, message_type);
@@ -109,6 +228,28 @@ void getAsPositiveInt(const char* message_type, int* value)
 	}
 }
 
+/*
+ * The method requests the user to input some string, and return its input as a string
+ * @message - a message presented to the user before the input
+ * @return - the string that the user inputed
+ */
+char* getAsString(const char* message)
+{
+	char* response = (char*)safeCalloc(1024,sizeof(char));
+	printf(message);
+	flushNull
+	scanf("%s", response);
+	flushNull
+
+	return response;
+}
+
+/*
+ * The method generated the file names into the allSettings variable, using the given parameters
+ *@folderpath - a relative folder path
+ *@image_prefix - the suffix of the images
+ *@image_prefix - the prefix of the images
+ */
 void generateFileNames(char* folderpath,char* image_prefix,char* image_suffix){
 	char* file_path;
 
@@ -124,6 +265,14 @@ void generateFileNames(char* folderpath,char* image_prefix,char* image_suffix){
 	}
 }
 
+/*
+ * The method create an imageData structure for a given image and properties (histogram and sift
+ * descriptors).
+ * @filePath - the path of the image
+ * @nBins - the number of bins for the RGB Histogram
+ * @maxNumFeatures - the maximum number of sift descriptors for the image
+ * @returns - a pointer to a new structure item with the relevant data
+ */
 imageData* calcImageData(char* filePath, int nBins, int maxNumFeatures)
 {
 	imageData* data = (imageData*)safeCalloc(1, sizeof(imageData));
@@ -139,7 +288,12 @@ imageData* calcImageData(char* filePath, int nBins, int maxNumFeatures)
 	return data;
 }
 
-//This method calculates the histograms and the sift descriptors
+
+/*
+ * This method calculate the histogram and sift
+ * descriptors data for each on of the images that were loaded to the settings structure
+ * it also allocated the shared imageData variable to be an array of pointers to the data of each image
+ */
 void calcHistAndSIFTDesc()
 {
 	int i;
@@ -153,49 +307,64 @@ void calcHistAndSIFTDesc()
 	}
 }
 
+/*
+ * This method gets the data needed for constructing the database items and generated the file names
+ */
 void setInput()
 {
 	allSettings = (settings*)safeCalloc(1, sizeof(settings));
-
-	char folderpath[MAX_IMAGE_PATH_LENGTH];
-	char image_prefix[MAX_IMAGE_PATH_LENGTH];
-	char image_suffix[MAX_IMAGE_PATH_LENGTH];
+	char *folderpath, *image_prefix, *image_suffix;
 
 	//Handle input
-	printf(ENTER_IMAGES_DIRECTORY_PATH);
-	flushNull
-	scanf("%s", folderpath);
-	flushNull
-
-	printf(ENTER_IMAGES_PREFIX);
-	flushNull
-	scanf("%s", image_prefix);
-	flushNull
-
+	folderpath = getAsString(ENTER_IMAGES_DIRECTORY_PATH);
+	image_prefix = getAsString(ENTER_IMAGES_PREFIX);
 	getAsPositiveInt(IMAGES, &(allSettings->numOfImages));
-
-	printf(ENTER_IMAGES_SUFFIX);
-	flushNull
-	scanf("%s", image_suffix);
-	flushNull
-
+	image_suffix = getAsString(ENTER_IMAGES_SUFFIX);
 	getAsPositiveInt(BINS, &(allSettings->numOfBins));
 	getAsPositiveInt(FEATURES, &(allSettings->numOfFeatures));
 
 	//generate files names
 	generateFileNames(folderpath, image_prefix, image_suffix);
+
+	//free allocated strings
+	if (folderpath != NULL)
+		free(folderpath);
+	if (image_prefix != NULL)
+		free(image_prefix);
+	if (image_suffix != NULL)
+		free(image_suffix);
+
 }
 
+/*
+ * This method is used for passing it as a parameter to the printArraysTopItems,
+ * it gets the value from an array of integers pointers
+ * @array - an array of key values pointers
+ * @index - the index we wish to return its key
+ */
 int getKeyFromIntArray(void* array, int index)
 {
 	return ((int*)array)[index];
 }
 
+/*
+ * This method is used for passing it as a parameter to the printArraysTopItems,
+ * it gets the key from an array of keyValue's pointers
+ * @array - an array of key values pointers
+ * @index - the index we wish to return its key
+ */
 int getKeyFromkeyValueArray(void* array, int index)
 {
 	return ((keyValue**)array)[index]->key;
 }
 
+/*
+ * The method gets an array and print the "index value" of its top NUM_OF_BEST_DIST_IMGS items
+ * the "index value" is retrieved using the function parameter funcGetKeyByIndex since the array is of general type.
+ * @topItems - the array of items
+ * @funcGetKeyByIndex - a function pointer that gets an array and an index and return the "index value"
+ * @message - a message that is printed pre-printing the array items
+ */
 void printArraysTopItems(void* topItems, int (*funcGetKeyByIndex)( void*, int), const char* message) {
 	int i;
 	printf(message);
@@ -209,6 +378,13 @@ void printArraysTopItems(void* topItems, int (*funcGetKeyByIndex)( void*, int), 
 	}
 }
 
+/*
+ * The method gets an array of top NUM_OF_BEST_DIST_IMGS keyValues (as pointers)
+ * and insert a new item if it has place among the best NUM_OF_BEST_DIST_IMGS while keeping the array sorted
+ * @index - the index of the keyValue item
+ * @distance - the value of the keyValue item
+ * @items - the array of pointers sorted by distance ascending
+ */
 void importSorted(int index, double distance, keyValue** items)
 {
 	int limit = index, j, k;
@@ -225,10 +401,12 @@ void importSorted(int index, double distance, keyValue** items)
 	//find where (if needed) should we insert the current item
 	for (j=0; j < limit && distance > items[j]->value ;j++);
 
+	//fix off by 1 problem when shifting the rest of the items
 	if (limit < NUM_OF_BEST_DIST_IMGS)
 		limit++;
 
-	if (j < NUM_OF_BEST_DIST_IMGS) //push at j
+	// we should insert the new item, at the j'th position
+	if (j < NUM_OF_BEST_DIST_IMGS)
 	{
 		//push new item
 		temp1 = items[j];
@@ -252,6 +430,10 @@ void importSorted(int index, double distance, keyValue** items)
 	}
 }
 
+/**
+ * Free the memory of the allocated items in the global comparison process.
+ * @topItems - an array of pointers of keyValue type
+ */
 void freeDataForGlobalFeatures(keyValue** topItems)
 {
 	int i;
@@ -266,6 +448,12 @@ void freeDataForGlobalFeatures(keyValue** topItems)
 	}
 }
 
+/**
+ * The method gets an image to compare with,
+ * it compares the image to the images database according to global features (RGB Histogram)
+ * it prints out the top "NUM_OF_BEST_DIST_IMGS" (see macro) indexes of images found similar
+ * @workingImageData - a pointer to the working image data
+ */
 void compareGlobalFeatures(imageData* workingImageData )
 {
 	int i;
@@ -285,7 +473,13 @@ void compareGlobalFeatures(imageData* workingImageData )
 	freeDataForGlobalFeatures(topItems);
 }
 
-void freeDataFromLocalDescProcess(int* counterArray, int* topItems, int*resultsArray) {
+/**
+ * Free the memory of the allocated items in the local descriptors process.
+ * @counterArray - integer array used in the local compare method
+ * @topItems - integer array used in the local compare method
+ * @resultsArray - integer array used in the local compare method
+ */
+void freeDataFromLocalDescProcess(int* counterArray, int* topItems, int* resultsArray) {
 	if (resultsArray != NULL)
 		free(resultsArray);
 
@@ -296,7 +490,12 @@ void freeDataFromLocalDescProcess(int* counterArray, int* topItems, int*resultsA
 		free(counterArray);
 }
 
-// sort the index counter array and get the best NUM_OF_BEST_DIST_IMGS
+/**
+ * The method gets an array of counters and returns an array of the best
+ * (desc order) NUM_OF_BEST_DIST_IMGS of the indexes of the original array,
+ * @counterArray - integer array of counters
+ * @returns - an array of the top NUM_OF_BEST_DIST_IMGS indexes of the original array
+ */
 int* getTopItems(int* counterArray) {
 	int i,j, tempMaxIndex;
 	int *topItems = (int*)safeCalloc(NUM_OF_BEST_DIST_IMGS, sizeof(int));
@@ -315,10 +514,9 @@ int* getTopItems(int* counterArray) {
 }
 
 /**
- * The method gets the database of images and an image to compare with,
- * it compares the image to database according to local features (sift descriptors)
+ * The method gets an image to compare with,
+ * it compares the image to the images database according to local features (sift descriptors)
  * it prints out the top "NUM_OF_BEST_DIST_IMGS" (see macro) indexes of images found similar
- * @param imagesData - the database as an array of pointers to an image data
  * @workingImageData - a pointer to the working image data
  */
 void compareLocalFeatures(imageData* workingImageData)
@@ -362,81 +560,11 @@ void compareLocalFeatures(imageData* workingImageData)
 	freeDataFromLocalDescProcess(counterArray, topItems, resultsArray);
 }
 
-/**
- * The method gets an image and frees the memory allocated to each one of its parts, and than free the memory for his pointer.
- * @image - a pointer to the image data
- */
-void freeImageData(imageData* image)
-{
-	int i;
-	if (image != NULL)
-	{
-		//free the rgbHist matrix
-		if (image->rgbHist != NULL)
-		{
-			for (i=0;i<3;i++)
-			{
-				free(image->rgbHist[i]);
-			}
-			free(image->rgbHist);
-		}
-		//free the sift descriptors matrix
-		if (image->siftDesc != NULL)
-		{
-			for (i=0;i<image->nFeatures;i++)
-			{
-				free(image->siftDesc[i]);
-			}
-			free(image->siftDesc);
-		}
-		//free the pointer to the image
-		free(image);
-	}
-}
+
 
 /**
- * The method gets the database of images and frees the memory allocated to each image, and to the array itself
- * @imagePath - the image path as string, pointer to a char array
- */
-void freeImages()
-{
-	int i;
-	if (imagesData != NULL)
-	{
-		for (i=0;i<allSettings->numOfImages;i++)
-		{
-			freeImageData(imagesData[i]);
-		}
-	}
-	free(imagesData);
-}
-
-void freeDatabase()
-{
-	if (allImagesDatabase != NULL)
-	{
-		if (allImagesDatabase->databaseFeatures != NULL)
-		{
-			free(allImagesDatabase->databaseFeatures);
-		}
-		if (allImagesDatabase->featuresPerImage != NULL)
-		{
-			free(allImagesDatabase->featuresPerImage);
-		}
-		free(allImagesDatabase);
-	}
-}
-
-
-void freeImagesAndDatabase(){
-	freeDatabase();
-	freeImages();
-}
-
-/**
- * The method gets the database of images and a path to an image,
- * it loads the image, compare it to the database images and ouptuts the comparison results.
- * @param imagesData - the database as an array of pointers to an image data
+ * The method gets a path to an image,
+ * it loads the image, compare it to the database images and ouptputs the comparison results.
  * @imagePath - the image path as string, pointer to a char array
  */
 void searchSimilarImages(char* imagePath)
@@ -444,7 +572,8 @@ void searchSimilarImages(char* imagePath)
 	//process the current new image
 	imageData* workingImageData = calcImageData(imagePath, allSettings->numOfBins, allSettings->numOfFeatures);
 
-	//verify database is built
+	//verify database is built (logically we could build the database after setting the
+	//							images paths but if the user will choose to exit at first run would than work for nothing)
 	if (allImagesDatabase == NULL)
 		setImagesDatabase();
 
@@ -459,32 +588,20 @@ void searchSimilarImages(char* imagePath)
 }
 
 /**
- * The method gets a pointer to a string, requests the user for an image path and update the string accordingly
- * @param workingImagePath - the pointer to the path
- */
-void setPathFromUser(char* workingImagePath) {
-	printf(ENTER_A_QUERY_IMAGE_OR_TO_TERMINATE);
-	flushNull
-	scanf("%s", workingImagePath);
-	flushNull
-}
-
-/**
  * The method interacts with the user and outputs comparision's data between the users input image path and the database.
  * the interaction ends when "#" input is entered
  */
 void startInteraction()
 {
 	//first run must always happen
-	char* workingImagePath = (char*)safeCalloc(MAX_IMAGE_PATH_LENGTH,sizeof(char));
-
-	setPathFromUser(workingImagePath);
+	char* workingImagePath = getAsString(ENTER_A_QUERY_IMAGE_OR_TO_TERMINATE);
 
 	// iterating until the user inputs "#"
 	while (strcmp(workingImagePath,"#"))
 	{
 		searchSimilarImages(workingImagePath);
-		setPathFromUser(workingImagePath);
+		free(workingImagePath);
+		workingImagePath = getAsString(ENTER_A_QUERY_IMAGE_OR_TO_TERMINATE);
 	}
 
 	//free the memory allocated for the path string
